d92cee8606536f047c74917654e332b7b751f0f8
Bhawna Agarwal <bhawna@umich.edu>
2020-04-19 17:08:21 -0700

    Update

--- goodreads.py
+++ goodreads.py
@@ -9,25 +9,28 @@ import requests
 import secrets # file that contains your OAuth credentials
 import xml.etree.ElementTree as ET
 
-def Book:
-    def __init__(self, name = "", author = "")
+class Book:
+    def __init__(self, name = "", author = ""):
         self.name = name
         self.author = author
 
-    def __str__(self)
+    def __str__(self):
         return f"{self.name} by {self.author}"
 
 class Goodreads:
-    def __init__(self, name = ""):
-        self.name = ""
+    def __init__(self):
+        client_key = secrets.GOODREADS_API_KEY
+        client_secret = secrets.GOODREADS_API_SECRET
+        self.auth = OAuth1(client_key, client_secret)
 
-    def get_all_bookshelves():
+    def get_all_bookshelves(self):
         #Returns a list of all the bookshelves of the current user
         pass
 
-    def get_all_books_in_shelf(shelf_name):
+    def get_all_books_in_shelf(self, shelf_name):
         #Returns a list of object Book
         pass
 
-    def get_googlebooks_link_of_book(book):
+    def get_googlebooks_link_of_book(self, book):
         #Access the google API and get the url link and other info
+        pass
