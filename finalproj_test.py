ad5c263bcaeac717308a843cf57342d0eddc52f4
Bhawna Agarwal <bhawna@umich.edu>
2020-04-19 19:14:19 -0700

    Name changes

--- finalproj_test.py
+++ finalproj_test.py
@@ -5,25 +5,25 @@
 
 import unittest
 import json
-import proj1
+import finalproj
 
 ###### Part 1 ######
 ####################
 
 class TestGoodreads(unittest.TestCase):
     def testgetbookshelves(self):
-        g1 = proj1.Goodreads('20227451')
+        g1 = finalproj.Goodreads('20227451')
         self.assertEqual(g1.get_all_bookshelves(), ["read", "currently-reading", "to-read"])
     
     def testgetbookfromshelf(self):
         #Test to make sure each element is of class Book
-        g1 = proj1.Goodreads('20227451')
+        g1 = finalproj.Goodreads('20227451')
         for book in g1.get_all_books_in_shelf("to-read"):
-            self.assertIsInstance(book, proj1.Book)
+            self.assertIsInstance(book, finalproj.Book)
 
 class TestBook(unittest.TestCase):
     def teststr1(self):
-        b1 = proj1.Book("Sapiens", ["Yuval Noah Harari"])
+        b1 = finalproj.Book("Sapiens", ["Yuval Noah Harari"])
         self.assertEqual(f"{b1}" , "Sapiens by Yuval Noah Harari")
         
 
